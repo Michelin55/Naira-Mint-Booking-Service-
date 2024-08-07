0e114544bc52737fc8bc39b5ccc936c5f499ffd0
Michelin <oluwabusiola@gmail.com>
2024-08-07 10:13:53 +0100

    Initial commit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,81 +1,65 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { Container, Row, Form, Button, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import './Login.css';
 import { Link } from 'react-router-dom';
-
 const Login = ({ onLogin }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null); 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://naira-mint-booking-service.onrender.com/login', {
+      const response = await axios.post('http://localhost:5000/login', {
         email,
         password,
       });
       console.log(response.data);
       localStorage.setItem('token', response.data.token);
-      onLogin(); 
-      navigate('/bookingForm'); 
+      onLogin(); // Notify parent component that login is successful
+      navigate('/bookingForm'); // Navigate to bookingForm after successful login
     } catch (error) {
-      console.error('Error:', error);
-      if (error.response) {
-        // Server responded with a status code outside of 2xx range
-        setError(error.response.data.message); // Set error message from server response
-      } else if (error.request) {
-        // The request was made but no response was received
-        setError('Server is unreachable. Please try again later.'); // Set custom error message for server unreachable
-      } else {
-      
-        setError('An unexpected error occurred. Please try again.'); 
-      }
+      console.error('error', error.response);
     }
   };
 
   return (
     <Container className="login-container">
       <Row className="justify-content-center">
-        <div className="login-form">
-          <h2 className="text-center">Login</h2>
-          {error && <Alert variant="danger">{error}</Alert>} {/* Render error message if error exists */}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </Form.Group>
+       
+          <div className="login-form">
+            <h2 className="text-center">Login</h2>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
 
-            <Button variant="primary" type="submit" block>
-              Login
-            </Button>
-          </Form>
-          <div className="text-center mt-3">
-            <p>
-              Don't have an account? <Link to="/register">Click here to register</Link>
-            </p>
+              <Button variant="primary" type="submit" block>
+                Login
+              </Button>
+            </Form>
+            <div className="text-center mt-3">
+              <p>Dont have an account? <Link to="/register">Click here to register</Link></p>
+            </div>
           </div>
-        </div>
       </Row>
     </Container>
   );
