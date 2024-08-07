bc38ac398f4102073f0ed8b73b34b34c330bb7c0
Michelin <oluwabusiola@gmail.com>
2024-08-07 10:39:50 +0100

    Initial commit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -12,7 +12,7 @@ const Login = ({ onLogin }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/login', {
+      const response = await axios.post('https://naira-mint-booking-service.onrender.com/login', {
         email,
         password,
       });
